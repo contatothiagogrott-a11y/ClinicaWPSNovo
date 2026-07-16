74a0225597bb4c09e31ae0bad39cfd229e476698
contatothiagogrott-a11y <contato.thiagogrott@gmail.com>
2026-07-16 14:42:14 -0300

    Change import of app to use .js extension
    Updated import statement to use .js extension for app.

--- api/[...slug].ts
+++ api/[...slug].ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import app from "./_lib/app";
+import app from "./_lib/app.js";
 
 // A Vercel roteia qualquer chamada a /api/* para esta função (nome de arquivo
 // com [...slug] = "catch-all"). Todo o roteamento real acontece dentro do
